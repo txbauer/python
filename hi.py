456ec93edc665c52e13134fecce3d0c6488aa774
txbauer <txbuer@gmail.com>
2022-02-05 15:17:33 -0600

    Initial commit

---
+++ hi.py
@@ -0,0 +1,18 @@
+# import the necessary packages
+import sys
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import requests
+from skimage.metrics import structural_similarity as ssim
+
+print(sys.version)
+print(sys.executable)
+
+r = requests.get('https://cnn.com')
+print(r.status_code)
+
+# name = input("Your name? ")
+name = "Jerry"
+print("hi, ", name)
